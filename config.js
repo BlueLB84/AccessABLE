c94c2ce679b557a74cb5fcc37e668e4ff30f497e
L Barbee <bluelb84@gmail.com>
2017-10-13 14:01:48 -0400

    testing travisCI issue

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
 					   global.DATABASE_URL ||
-					   'mongodb://localhost:27017/accessable';
+					   'mongodb://localhost/accessable';
 exports.TEST_DATABASE_URL = (
 	process.env.TEST_DATABASE_URL ||
-	'mongodb://localhost:27017/test-accessable');
+	'mongodb://localhost/test-accessable');
 exports.PORT = process.env.PORT || 8080;
