fac7c734b032e2e346ae79b0078d2cf311649451
L Barbee <bluelb84@gmail.com>
2017-10-13 16:39:52 -0400

    removed tabs and spacing

--- config.js
+++ config.js
@@ -1,7 +1,3 @@
-exports.DATABASE_URL = process.env.DATABASE_URL ||
-					   global.DATABASE_URL ||
-					   'mongodb://localhost/accessable';
-exports.TEST_DATABASE_URL = (
-	process.env.TEST_DATABASE_URL ||
-	'mongodb://localhost/test-accessable');
+exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/accessable';
+exports.TEST_DATABASE_URL = (process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-accessable');
 exports.PORT = process.env.PORT || 8080;
