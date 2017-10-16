4ac5e83ceb1e726a8013ffac1604a52efa8ba426
L Barbee <bluelb84@gmail.com>
2017-10-16 17:04:51 -0400

    set up additional routers

---
+++ routers/userRouter.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+mongoose.Promise = global.Promise;
+
+const userGetIdRouter = require('./userRouters/userGetIdRouter');
+const userPostRouter = require('./userRouters/userPostRouter');
+const userDeleteRouter = require('./userRouters/userDeleteRouter');
+
+router.get('/:id', userGetIdRouter);
+router.post('/', jsonParser, userPostRouter);
+router.put('/:id', jsonParser, userPutRouter);
+router.delete('/:id', userDeleteRouter);
+
+module.exports = router;
