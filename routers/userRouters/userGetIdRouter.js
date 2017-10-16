ad60fa10243a1b7f9d08f977c0b2a0ca82bd2caa
L Barbee <bluelb84@gmail.com>
2017-10-16 17:06:37 -0400

    initial set up of each endpoint

---
+++ routers/userRouters/userGetIdRouter.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const {User} = require('../../user-models');
+
+module.exports = function(req, res) {
+	User
+		.findById(req.params.id)
+		.then(user => res.json(user.userApiRep()))
+		.then(res.status(200))
+		.catch(err => {
+			console.error(err);
+				res.status(500).json({message: 'Internal server error'});
+		});
+};
