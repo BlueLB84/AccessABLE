7bea9cb6aec6149e3f714c2152d830dd854e9147
L Barbee <bluelb84@gmail.com>
2017-11-30 22:18:07 -0500

    testing passport auth

--- middlewares/is-logged-in-middleware.js
+++ middlewares/is-logged-in-middleware.js
@@ -1,11 +1,17 @@
 // check to see if user is logged in
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
 
 module.exports = function loggedIn(req, res, next) {
-    if (req.data.username) {
-        next();
-    } else {
-        res.redirect('/login');
-    }
+    passport.authenticate('jwt', {session: false});
+    next();
+
+    // if (req.data.username) {
+    //     next();
+    // } else {
+    //     res.redirect('/login');
+    // }
 }
 
 
